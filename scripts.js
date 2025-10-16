c60152f3009896a657dde9882443499af821a7f0
fathimashabnam200-sys <fathimashabnam200@gmail.com>
2025-10-17 00:06:39 +0530

    Revert "Update and rename scripts.css to scripts.js"

--- scripts.js
+++
@@ -1,8 +0,0 @@
-// Smooth scroll navigation
-document.querySelectorAll("nav a").forEach(link => {
-  link.addEventListener("click", function(e) {
-    e.preventDefault();
-    const target = document.querySelector(this.getAttribute("href"));
-    target.scrollIntoView({ behavior: "smooth" });
-  });
-});
